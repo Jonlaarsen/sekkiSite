5147e12933e3de490bf2600f309ae126b24644c8
Jonlaarsen <jontoftdallarsen@icloud.com>
2025-01-07 22:52:08 +0900

    test2final

--- src/app/components/Categories.jsx
+++ src/app/components/Categories.jsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 const Categories = () => {
   return (
-    <div className="h-full">
-      <div className="flex flex-col md:flex-row gap-4 w-screen h-screen items-center justify-center pb-1 md:pb-10">
+    <div className="h-full w-screen pt-10">
+      <div className="grid grid-cols-1 md:grid-cols-3">
         {Catagories.map((item) => (
           <div
             key={item.id}
@@ -14,7 +14,7 @@ const Categories = () => {
               id={item.id}
               key={item.id}
               style={{ backgroundImage: `url(${item.thumbnail})` }}
-              className="flex group opacity-60 hover:opacity-100 items-center justify-center w-[20rem] bg-red-600 h-[10rem] md:w-[29rem] md:h-[35rem] bg-cover bg-center"
+              className="flex object-fill group opacity-60 hover:opacity-100 items-center justify-center bg-red-600 h-[13rem]  md:h-[35rem] bg-cover bg-center"
             >
               <h1 className="text-[1rem] md:text-4xl opacity-100 group-hover:opacity-0">
                 {item.title}
