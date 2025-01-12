c365cce32008a0427758f0bd4aee15a01e09404b
Jonlaarsen <jontoftdallarsen@icloud.com>
2025-01-12 21:19:02 +0900

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    
+        images: {
+          domains: ['tommychoi.s3.ap-northeast-2.amazonaws.com'],
+        },
+};
 
 export default nextConfig;
